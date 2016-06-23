3f6802ffea9bb494ef633cddf7fc2e12bb07328b
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-06-23 09:53:19 +0100

    Added a feature which allows building libsodium from source.

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -1,15 +1,17 @@
-use std::env;
+#[cfg(not(feature = "build-libsodium"))]
 extern crate pkg_config;
 
+#[cfg(not(feature = "build-libsodium"))]
 fn main() {
+    use std::env;
 
     if let Ok(lib_dir) = env::var("SODIUM_LIB_DIR") {
 
-    	println!("cargo:rustc-flags=-L native={}", lib_dir);
+        println!("cargo:rustc-flags=-L native={}", lib_dir);
 
         let mode = match env::var_os("SODIUM_STATIC") {
             Some(_) => "static",
-            None => "dylib"
+            None => "dylib",
         };
         println!("cargo:rustc-flags=-l {0}=sodium", mode);
 
@@ -19,4 +21,26 @@ fn main() {
 
     }
 
+}
+
+
+
+#[cfg(feature = "build-libsodium")]
+extern crate gcc;
+
+#[cfg(feature = "build-libsodium")]
+fn main() {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    let mut gcc_config = gcc::Config::new();
+    let _ = gcc_config.include("libsodium/src/libsodium/include/sodium");
+
+    let file = File::open("sources.txt").unwrap();
+    let file_reader = BufReader::new(file);
+
+    for line in file_reader.lines() {
+        let _ = gcc_config.file(line.unwrap());
+    }
+    let _ = gcc_config.compile("libsodium.a");
 }
